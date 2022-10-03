b348486e0a1b274b17f386000b31ea3ece86c54a
Bailey Cage <bailey.cage@gmail.com>
2022-10-03 14:41:06 -0400

    create shell endpoints with return messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,70 @@ class ApplicationController < Sinatra::Base
     { message: "Good luck with your project!" }.to_json
   end
 
+  #############
+  # CUSTOMERS #
+  #############
+
+  get "/customers" do
+    { message: "list all customers" }.to_json
+  end
+
+  get "/customers/:id" do
+    { message: "list 1 customer" }.to_json
+  end
+
+  patch "/customers/:id/edit" do
+    { message: "edit customer" }.to_json
+  end
+
+  delete "/customers/:id/delete" do
+    { message: "delete customer" }.to_json
+  end
+
+  ##########
+  # MOVIES #
+  ##########
+
+  get "/movies" do
+    { message: "list all movies" }.to_json
+  end
+
+  get "/movies/:id" do
+    { message: "list 1 movie" }.to_json
+  end
+
+  ###########
+  # RENTALS #
+  ###########
+
+  # read all rentals
+  get "/rentals" do
+    { message: "list all rentals" }.to_json
+  end
+
+  # read 1 rental
+  get "/rentals/:id" do
+    { message: "list 1 rental" }.to_json
+  end
+
+  # create rental without data
+  get "/rentals/new" do
+    { message: "make new rental without data" }.to_json
+  end
+
+  # create rental with data
+  post "/rentals" do
+    { message: "make new rental with data" }.to_json
+  end
+  
+  # update rental
+  patch "/rentals/:id/edit" do
+    { message: "edit rental" }.to_json
+  end
+
+  # delete rental
+  delete "/rentals/:id/delete" do
+    { message: "delete rental" }.to_json
+  end
+
 end
