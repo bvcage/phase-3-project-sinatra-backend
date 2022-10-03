d3628c40b3ef3a8b78a1a157b167ff7ecc4179fc
Bailey Cage <bailey.cage@gmail.com>
2022-10-03 17:26:27 -0400

    add optional query params to get all

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,14 +3,20 @@ class MoviesController < ApplicationController
    # returns all movies
    get "/movies" do
       movies = Movie.all
-      { data: movies, status: 200 }
+      # optional query parameters
+      if params.length > 0
+         params.each do |var, val|
+            movies = movies.search(var, val)
+         end
+      end
+      { data: movies, status: 200 }.to_json
    end
   
    # returns 1 movie
    get "/movies/:id" do
       id = params[:id]
       movie = Movie.find(id)
-      { data: movie, status: 200 }
+      { data: movie, status: 200 }.to_json
    end
 
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,8 @@
 class Movie < ActiveRecord::Base
     has_many :rentals
     has_many :customers, through: :rentals
+
+    def self.search (data, value)
+        where("#{data} LIKE ?", "%#{value}%")
+    end
 end
