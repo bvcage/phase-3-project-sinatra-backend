5c225cec0859a543fff4fb592b063be84f1182a2
Bailey Cage <bailey.cage@gmail.com>
2022-10-03 13:31:39 -0400

    add additional movie seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,21 @@ IMDb_ID_LIST = [
    "tt3104988",   # Crazy Rich Asians
    "tt3846674",   # To All the Boys I've Loved Before
    "tt6710474",   # Everything Everywhere All at Once
+   "tt0120812",   # Rush Hour
+   "tt0096061",   # Scrooged
+   "tt1046997",   # Miracle at St Anna
+   "tt1745960",   # Top Gun Maverick
+   "tt2463208",   # The Adam Project
+   "tt0477348",   # No Country for Old Men
+   "tt7784604",   # Hereditary
+   "tt0109445",   # Clerks
+   "tt2911666",   # John Wick
+   "tt0081505",   # The Shining
+   "tt1375670",   # Grown Ups
+   "tt2788710",   # The Interview
+   "tt0099785",   # Home Alone
+   "tt0109686",   # Dumb and Dumber
+   "tt0092312",   # 21 Jump Street
 ]
 
 IMDb_ID_LIST.each do |imdb_id|
