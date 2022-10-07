9d7df2d3d8ee9626782789d3a9fef89fc742a510
Bailey Cage <bailey.cage@gmail.com>
2022-10-07 15:32:17 -0400

    add parameter for exact vs similar search

--- app/modules/searchable.rb
+++ app/modules/searchable.rb
@@ -1,7 +1,11 @@
 module Searchable
 
-   def search (data, value)
-      where("#{data} LIKE ?", "%#{value}%")
+   def search (data, value, exact = false)
+      if exact
+         where("#{data} = #{value}")
+      else
+         where("#{data} LIKE ?", "%#{value}%")
+      end
    end
 
 end
