7e768105b744f8eb3dd90b95e481391884fa759c
Bailey Cage <bailey.cage@gmail.com>
2022-10-07 15:33:02 -0400

    add endpoint for patch review, set movie_id param to be exact search

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,13 +6,17 @@ class ReviewsController < ApplicationController
       # filter based on optional parameters
       if params.length > 0
          params.each do |var, val|
-            reviews = reviews.search(var, val)
+            if var == "movie_id" then
+               reviews = reviews.search(var, val, true)
+            else
+               reviews = reviews.search(var, val)
+            end
          end
       end
       # join desired info
       reviews = reviews.map do |review|
-         puts review.customer
          {
+            id: review.id,
             stars: review.stars,
             comment: review.comment,
             movie: {
@@ -36,4 +40,12 @@ class ReviewsController < ApplicationController
       { data: review, status: 201 }.to_json
    end
 
+   # update review
+   patch "/reviews/:id" do
+      id = params[:id]
+      review = Review.find(id)
+      edits = JSON.parse(request.body.read)
+      review.update(edits)
+      { data: review, status: 200 }.to_json
+   end
 end
